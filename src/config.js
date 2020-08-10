21a7f706266c7f4de217259b39d8eb37f3340be2
rsharm18 <40524914+rsharm18@users.noreply.github.com>
2020-08-10 16:13:47 -0500

    setting up the react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-api-sharmaji"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://8fb3dytu5f.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_JBgjs5YxV",
+        APP_CLIENT_ID: "6n0ndej3nuo4kqsmpol9jprmlu",
+        IDENTITY_POOL_ID: "us-east-2:5aadc315-6a97-48fa-8003-67aeb245ac0b"
+    }
+};
