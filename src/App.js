2e068e0909f1ca75c71261ca97d7351a1f8ce6b7
rsharm18 <40524914+rsharm18@users.noreply.github.com>
2020-08-11 12:31:35 -0500

    adding error reporting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
 import { Link, useHistory } from "react-router-dom";
 import { onError } from "./libs/errorLib";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
 
@@ -71,11 +72,11 @@ function App() {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <AppContext.Provider
-          value={{ isAuthenticated, userHasAuthenticated }}
-        >
-          <Routes />
-        </AppContext.Provider>
+        <ErrorBoundary>
+          <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+            <Routes />
+          </AppContext.Provider>
+        </ErrorBoundary>
       </div>
     )
   );
--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,12 +1,41 @@
-export function onError(error) {
-    let message = error.toString();
+import * as Sentry from "@sentry/browser";
 
-    console.log(` Error === ${error}`);
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.init({ dsn: "https://4e6b2bede08a4ec29c3722822a40a3bc@o432819.ingest.sentry.io/5386863" });
+}
+
+export function logError(error, errorInfo = null) {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.withScope((scope) => {
+        errorInfo && scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    });
+}
+
+export function onError(error) {
+    let errorInfo = {};
+    let message = error.toString();
 
     // Auth errors
     if (!(error instanceof Error) && error.message) {
+        errorInfo = error;
         message = error.message;
+        error = new Error(message);
+        // API errors
+    } else if (error.config && error.config.url) {
+        errorInfo.url = error.config.url;
     }
+
+    logError(error, errorInfo);
 
     alert(message);
 }
